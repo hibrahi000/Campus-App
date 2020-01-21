536b07051c3c6215b21ac06297d73ce766744503
Hashmat Ibrahimi <Hashmatibrahimi0711@gmail.com>
2020-01-20 19:49:02 -0500

    added routes for the websites and gave mock display for group to utilize the data passed in properly

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -4,28 +4,45 @@ const axios = require('axios');
 
 class App extends Component {
 	state = {
-		data: null
+		data: []
 	};
 
 	componentDidMount() {
 		// Call our axios function below once the component mounts
-		this.callBackendAPI().then((res) => this.setState({ data: res.express })).catch((err) => console.log(err));
+		this.callBackendAPI()
+			.then((res) => {
+				res.campusData.forEach((elem) => {
+					console.log(elem.university);
+				});
+				this.setState({ data: res.campusData });
+			})
+			.catch((err) => console.log(err));
 	}
 	callBackendAPI = async () => {
 		//this is a axios call that calls to the proxy with the url /login_backend that goes to express routes
-		return await axios.get('/Student').then((data) => {
-			console.log(data.data, 'this is the data');
+		return await axios.get('/Campus').then((data) => {
+			console.log(data.data.campusData, 'this is the data');
 			return data.data;
 		});
 	};
 
 	render() {
+		let data = JSON.stringify(this.state.data);
+
+		let plist = this.state.data.map((campus) => (
+			<div key={campus.id}>
+				<h1>{campus.university}</h1>
+				<p>{campus.id}</p>
+				<p>{campus.location}</p>
+				<p>{campus.number_of_students}</p>
+			</div>
+		));
 		return (
 			<div className="App">
 				<header className="App-header">
 					<h1 className="App-title">Welcome to React</h1>
 					// Render the newly fetched data inside of this.state.data
-					<p>{this.state.data}</p>
+					{plist}
 				</header>
 			</div>
 		);
--- middleware/campusMiddleware.js
+++ middleware/campusMiddleware.js
@@ -3,9 +3,13 @@ const { getStudentById } = require('../api/studentDB');
 
 exports.campusPage_load = (req, res, next) => {
 	console.log('Loading Login Page');
-	res.send({ express: 'YOUR CAMPUS PAGE' });
-	// next();
+	getAllCampuses().then((data) => {
+		res.send({ campusData: data });
+	});
 };
+
+exports.createCampus;
+
 exports.test_load = (req, res, next) => {
 	// getStudentById(1).then((data) => console.log(data));
 	// getAllCampuses().then(data => {
