132411c19bb5062b01064dc9ba7a84ac6dcaac47
Ikuseghan Pryce <yardgal21@yahoo.com>
2020-01-20 19:53:58 -0500

    Updates to the contact page

--- src/contact.js
+++ src/contact.js
@@ -2,9 +2,25 @@ import React, {Component} from 'react';
 import './App.css';
 
 class contact extends Component {
+    
     render() {
-        return null;
-    }
-}
+        return (
+              <form>
+                <h1><strong>Contact Us:</strong></h1>
+                <p>Enter your name:</p>
+                <input type="text"/>
+
+                <p>Information Inquiry:</p>
+                <textarea placeholder="Enter submission"/>
+
+                <input type="submit" />
+
+              </form>
+            );
+          }
+        }
+    ReactDOM.render(<contact />, document.getElementById('root'));
+    
+
 
 export default contact;
